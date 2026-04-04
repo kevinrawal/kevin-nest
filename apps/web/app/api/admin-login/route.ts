be524f11d32a5d97cdde98d7dd650f6038692516
KevinRawal <kevinrawal30@gmail.com>
2026-04-04 18:13:27 +0530

    testing...

--- apps/web/app/api/admin-login/route.ts
+++ apps/web/app/api/admin-login/route.ts
@@ -1,8 +1,11 @@
 import { NextResponse } from 'next/server'
 
 export async function POST(req: Request) {
-  const { password } = await req.json()
+  try{
+    const { password } = await req.json()
 
+  console.log('Received password:', password)
+  console.log('Expected:', process.env.ADMIN_PASSWORD)
   if (password === process.env.ADMIN_PASSWORD) {
     const response = NextResponse.json({ success: true })
 
@@ -10,6 +13,7 @@ export async function POST(req: Request) {
         httpOnly: true,
         secure: true,
         path: '/',
+        sameSite: 'lax',
         maxAge: 60 * 60,
     })
 
@@ -17,4 +21,9 @@ export async function POST(req: Request) {
   }
 
   return NextResponse.json({ error: 'Invalid password' }, { status: 401 })
+  }
+  catch (error) {
+    console.error('Error processing admin login:', error)
+    return NextResponse.json({ error: 'Internal server error' }, { status: 500 })
+  }
 }
