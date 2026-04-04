e336280ee6dd97e55006dff1b0ebc7546840a669
KevinRawal <kevinrawal30@gmail.com>
2026-04-04 16:37:38 +0530

    minor fixes

--- apps/web/data/blog.ts
+++ apps/web/data/blog.ts
@@ -8,26 +8,4 @@ export interface BlogPost {
   content: string // Markdown content
 }
 
-export const blogPosts: BlogPost[] = [
-  {
-    id: 1,
-    title: "Welcome to My Blog",
-    slug: "welcome-to-my-blog",
-    headerImage: "/imgs/blog/img-1.jpg",
-    date: "2024-01-01",
-    tags: ["introduction", "personal"],
-    content: `# Welcome to My Blog
-
-This is the first post on my personal blog. I'm excited to share my thoughts and experiences.
-
-## What to Expect
-
-- Technical articles
-- Personal reflections
-- Project updates
-
-![Sample Image](/imgs/blog/img-1.jpg)
-
-Stay tuned for more content!`
-  },
-]
+export const blogPosts: BlogPost[] = []
